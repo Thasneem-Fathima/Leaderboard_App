bfda9bd6ec064f1547b3b8a9469996dda3eaf2db
Alamelu515 <alamelulakshmi515@gmail.com>
2024-10-14 14:05:40 +0530

    Committed changes made for implementing task 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,12 +18,20 @@ export class AppComponent implements OnInit {
 
   topProfiles: UserProfile[] = [];
   remainingProfiles: UserProfile[] = [];
+  filteredProfiles: UserProfile[] = [];
+  searchText: string='';
+  lastUpdatedDate: string='';
   
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.readCSV();
     this.triggerPartyPopperAnimation(); // Call the function to trigger animation
+  }
+
+  clearSearch(){
+    this.searchText='';
+    this.filterProfiles();
   }
 
   // Function to trigger party popper animation on page load
@@ -40,6 +48,8 @@ export class AppComponent implements OnInit {
   }
 
   readCSV() {
+    const fileName = 'Madras Institute of Technology - Chennai, India [07 Oct].csv'; // The filename with the date
+    this.extractLastUpdatedDate(fileName);
     this.http.get('assets/Madras Institute of Technology - Chennai, India [07 Oct].csv', { responseType: 'text' }).subscribe(
       data => {
         this.parseCSV(data);
@@ -48,6 +58,15 @@ export class AppComponent implements OnInit {
         console.error('Error reading CSV file:', error);
       }
     );
+  }
+
+  extractLastUpdatedDate(fileName: string) {
+    const dateRegex = /\[(.*?)\]/; // Regex to capture the date inside brackets
+    const match = fileName.match(dateRegex);
+    
+    if (match && match[1]) {
+      this.lastUpdatedDate = match[1]; // Store the extracted date
+    }
   }
 
   parseCSV(csvData: string) {
@@ -66,18 +85,34 @@ export class AppComponent implements OnInit {
         profiles.sort((a, b) => b.badges - a.badges);
         this.topProfiles = profiles.slice(0, 3);
         this.remainingProfiles = profiles.slice(3);
+        this.filteredProfiles = [...this.remainingProfiles];
+        this.filterProfiles();
       }
     });
   }
 
-  
+  filterProfiles() {
+    if (!this.searchText) {
+        this.filteredProfiles = this.remainingProfiles;
+    } else {
+        const lowerCaseSearchText = this.searchText.toLowerCase();
+        this.filteredProfiles = this.remainingProfiles.filter(profile =>
+            profile.username.toLowerCase().includes(lowerCaseSearchText) ||
+            profile.email.toLowerCase().includes(lowerCaseSearchText) ||
+            profile.badges.toString().includes(lowerCaseSearchText)
+        );
+    }
+    this.pg = 1;
+    console.log(this.filteredProfiles);
+}
+ 
 //logic for pagination:
 pg: number = 1; // Current page
 fetchPages: number = 15; // Default number of items per page
 
 get paginatedProfiles() {
   const startIndex = (this.pg - 1) * this.fetchPages;
-  return this.remainingProfiles.slice(startIndex, startIndex + this.fetchPages);
+  return this.filteredProfiles.slice(startIndex, startIndex + this.fetchPages);
 }
 
 onPageChange(event: Event): void {
