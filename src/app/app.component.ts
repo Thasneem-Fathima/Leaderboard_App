3a0766cec10cbc12c333e5f766220a12141bd4c9
vijaisuria <98590895+vijaisuria@users.noreply.github.com>
2024-11-08 01:12:20 +0530

    Fix: Navbar and swags column

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,24 @@ interface UserProfile {
   skillsBoostUrl: string;
   badges: number;
   arcade_status: number;
-  rank:number
+  rank: number;
+  swag: boolean;
 }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-
   ProfilesList: UserProfile[] = [];
   // topProfiles: UserProfile[] = [];
   // swappedProfiles: UserProfile[] = [];
   // remainingProfiles: UserProfile[] = [];
   filteredProfiles: UserProfile[] = [];
-  searchText: string='';
-  lastUpdatedDate: string='';
-  prevDate: string='';
+  searchText: string = '';
+  lastUpdatedDate: string = '';
+  prevDate: string = '';
 
   days: number = 0;
   hours: number = 0;
@@ -33,7 +33,6 @@ export class AppComponent implements OnInit {
   seconds: number = 0;
   interval: any;
   eventTime = new Date('November 1, 2024 17:00:00 GMT+0530').getTime();
-
 
   constructor(private http: HttpClient) {}
 
@@ -43,8 +42,8 @@ export class AppComponent implements OnInit {
     this.triggerPartyPopperAnimation();
     this.extractLastUpdatedDate();
   }
-  clearSearch(){
-    this.searchText='';
+  clearSearch() {
+    this.searchText = '';
     this.filterProfiles();
   }
 
@@ -65,10 +64,10 @@ export class AppComponent implements OnInit {
     const fileName = 'GenAI.csv';
     // this.extractLastUpdatedDate(fileName);
     this.http.get('assets/GenAI.csv', { responseType: 'text' }).subscribe(
-      data => {
+      (data) => {
         this.parseCSV(data);
       },
-      error => {
+      (error) => {
         console.error('Error reading CSV file:', error);
       }
     );
@@ -85,7 +84,10 @@ export class AppComponent implements OnInit {
           skillsBoostUrl: row['Google Cloud Skills Boost Profile URL'],
           badges: parseInt(row['# of Skill Badges Completed'], 10) || 0,
           arcade_status: parseInt(row['# of Arcade Games Completed'], 10),
-          rank:0
+          rank: 0,
+          swag:
+            parseInt(row['# of Arcade Games Completed'], 10) == 1 &&
+            parseInt(row['# of Skill Badges Completed'], 10) >= 15,
         }));
 
         // profiles.sort((a, b) => b.badges - a.badges);
@@ -108,7 +110,10 @@ export class AppComponent implements OnInit {
         for (let i = 0; i < profiles.length; i++) {
           const profile = profiles[i];
 
-          if (profile.badges !== lastBadges || profile.arcade_status !== lastArcadeStatus) {
+          if (
+            profile.badges !== lastBadges ||
+            profile.arcade_status !== lastArcadeStatus
+          ) {
             currentRank = i + 1;
             lastBadges = profile.badges;
             lastArcadeStatus = profile.arcade_status;
@@ -122,32 +127,32 @@ export class AppComponent implements OnInit {
         // this.swappedProfiles[0] = this.swappedProfiles[1];
         // this.swappedProfiles[1] = temp;
 
-
         // this.remainingProfiles = profiles.slice(3);
         // this.filteredProfiles = [...this.remainingProfiles];
-        this.ProfilesList=profiles;
+        this.ProfilesList = profiles;
         this.filteredProfiles = [...this.ProfilesList];
         this.filterProfiles();
-      }
+      },
     });
   }
 
   extractLastUpdatedDate() {
-    this.http.get<{ buildDate: string }>('assets/build-metadata.json')
+    this.http
+      .get<{ buildDate: string }>('assets/build-metadata.json')
       .subscribe(
-        data => {
+        (data) => {
           const date = new Date(data.buildDate);
           const formattedDate = date.toLocaleDateString('en-GB', {
             day: '2-digit',
             month: '2-digit',
-            year: 'numeric'
+            year: 'numeric',
           });
 
           // Format the time (e.g., 06:30 PM)
           const formattedTime = date.toLocaleTimeString('en-US', {
             hour: '2-digit',
             minute: '2-digit',
-            hour12: true
+            hour12: true,
           });
 
           // Combine date and time
@@ -156,7 +161,7 @@ export class AppComponent implements OnInit {
           previous.setDate(date.getDate() - 1);
           this.prevDate = previous.toLocaleDateString('en-GB');
         },
-        error => {
+        (error) => {
           console.error('Error loading build date:', error);
         }
       );
@@ -164,87 +169,88 @@ export class AppComponent implements OnInit {
 
   filterProfiles() {
     if (!this.searchText) {
-        this.filteredProfiles = this.ProfilesList;
+      this.filteredProfiles = this.ProfilesList;
     } else {
-        const lowerCaseSearchText = this.searchText.toLowerCase();
-        this.filteredProfiles = this.ProfilesList.filter(profile =>
-            profile.username.toLowerCase().includes(lowerCaseSearchText) ||
-            profile.email.toLowerCase().includes(lowerCaseSearchText) ||
-            profile.badges.toString().includes(lowerCaseSearchText)
-        );
+      const lowerCaseSearchText = this.searchText.toLowerCase();
+      this.filteredProfiles = this.ProfilesList.filter(
+        (profile) =>
+          profile.username.toLowerCase().includes(lowerCaseSearchText) ||
+          profile.email.toLowerCase().includes(lowerCaseSearchText) ||
+          profile.badges.toString().includes(lowerCaseSearchText)
+      );
     }
     this.pg = 1;
     console.log(this.filteredProfiles);
   }
 
+  //logic for pagination:
+  pg: number = 1; // Current page
+  fetchPages: number = 5; // Default number of items per page
 
- //logic for pagination:
- pg: number = 1; // Current page
- fetchPages: number = 5; // Default number of items per page
-
- get paginatedProfiles() {
-   const startIndex = (this.pg - 1) * this.fetchPages;
-   return this.filteredProfiles.slice(startIndex, startIndex + this.fetchPages);
- }
-
- onPageChange(event: Event): void {
-   const selectElement = event.target as HTMLSelectElement;
-   this.fetchPages = +selectElement.value; // Update items per page
-   this.pg = 1; // Reset to the first page
- }
-
- nextPage(): void {
-   if (this.pg < this.totalPages) {
-     this.pg++;
-   }
- }
-
- previousPage(): void {
-   if (this.pg > 1) {
-     this.pg--;
-   }
- }
-
- get totalPages() {
-   return Math.ceil(this.filteredProfiles.length / this.fetchPages);
- }
-
- get pageNumbers() {
-   const pages = [];
-   for (let i = 1; i <= this.totalPages; i++) {
-     pages.push(i);
-   }
-   return pages;
- }
-
-
- goToPage(page: number) {
-   this.pg = page;
- }
-
-
- startCountdown() {
-  this.interval = setInterval(() => {
-    const now = new Date().getTime();
-    const timeLeft = this.eventTime - now;
-
-    this.days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    this.hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    this.minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    this.seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-
-    if (timeLeft < 0) {
-      clearInterval(this.interval);
-      this.days = this.hours = this.minutes = this.seconds = 0;
-    }
-  }, 1000);
-}
-
-ngOnDestroy() {
-  if (this.interval) {
-    clearInterval(this.interval);
+  get paginatedProfiles() {
+    const startIndex = (this.pg - 1) * this.fetchPages;
+    return this.filteredProfiles.slice(
+      startIndex,
+      startIndex + this.fetchPages
+    );
   }
-}
 
+  onPageChange(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    this.fetchPages = +selectElement.value; // Update items per page
+    this.pg = 1; // Reset to the first page
+  }
+
+  nextPage(): void {
+    if (this.pg < this.totalPages) {
+      this.pg++;
+    }
+  }
+
+  previousPage(): void {
+    if (this.pg > 1) {
+      this.pg--;
+    }
+  }
+
+  get totalPages() {
+    return Math.ceil(this.filteredProfiles.length / this.fetchPages);
+  }
+
+  get pageNumbers() {
+    const pages = [];
+    for (let i = 1; i <= this.totalPages; i++) {
+      pages.push(i);
+    }
+    return pages;
+  }
+
+  goToPage(page: number) {
+    this.pg = page;
+  }
+
+  startCountdown() {
+    this.interval = setInterval(() => {
+      const now = new Date().getTime();
+      const timeLeft = this.eventTime - now;
+
+      this.days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor(
+        (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      this.minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+      if (timeLeft < 0) {
+        clearInterval(this.interval);
+        this.days = this.hours = this.minutes = this.seconds = 0;
+      }
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
 }
